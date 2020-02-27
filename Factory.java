5c69081f6f242bd31b7e845d99baef06b111bd67
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-02-26 23:50:11 -0600

    Factory

---
+++ Factory.java
@@ -0,0 +1,21 @@
+class Factory<E> {
+    //selecciona la implementacion a utilizar para un stack
+    //se utiliza el patron Factory
+       public Pila<E> getStack(String entry) {
+        // seleccion de la implementacion a utilizar:
+        if (entry.equals("AL"))
+          return new Arraylist()<E>; //regresa ArrayList
+        else
+          return new VectorS<E>(); //regresa Vector
+       }
+
+       public List<E> getList(String entry){
+           if(entry.equals("SI")){
+               return new SinglyLinkedList<E>();
+           }else if(entry.equals("DO")){
+               return new DoublyLinkedList<E>();
+          }else{
+            return new CircularList()<E>;
+          }
+       }
+    }
